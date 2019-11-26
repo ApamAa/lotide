77306085c61806f722caf89485ebca4a8c7d964f
ApamAa <apama.jamshidi@gmail.com>
2019-11-26 04:30:08 +0000

    with emoji

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+let emoji = require('node-emoji');
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(emoji.emojify(':white_check_mark:') + " Assertion Passed : " + actual + " === " +    expected);
+  } else {
+    console.log(emoji.emojify(':red_circle:') + " Assertion Failed : " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
