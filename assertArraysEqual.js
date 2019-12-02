0a945e214726bd0fba6be9ba316a6bd8c3c35622
ApamAa <apama.jamshidi@gmail.com>
2019-12-02 23:19:50 +0000

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,9 @@
-const eqArrays = function(Array1,Array2){
-  farz = true;
-  if (Array1.length !== Array2.length){
-      farz = false;
-  } else {
-      for (i=0; i<Array1.length;i++)
-      if (Array1[i]!==Array2[i]) {
-          farz = false;
-        
-      }
-  
-  }
-  return farz;
-};
-const assertArrayEqual = function(firstArr, secondArr) {
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = function(firstArr, secondArr) {
   if (eqArrays(firstArr,secondArr) === false ){
       console.log(`⚡️⚡️ Assertion Failed: ${firstArr} !==  ${secondArr}`);
   }else {
       console.log(`✅✅ Assertion Passed: ${firstArr} ===  ${secondArr}`);
   }
 };
-assertArrayEqual([3,3,3],[3,3,3]);
+module.exports = assertArraysEqual
