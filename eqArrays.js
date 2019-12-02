fd425ed43d0bec8aab86f4f917eff31a5e27dfb1
ApamAa <apama.jamshidi@gmail.com>
2019-12-02 23:04:52 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,18 @@
-let emoji = require('node-emoji');
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(emoji.emojify(':white_check_mark:') + ` Assertion Passed :  ${actual}   ===   ${expected}`);
-  } else {
-    console.log(emoji.emojify(':red_circle:') + ` Assertion Failed : ${actual}  !==  ${expected}`);
-  }
 
-};
-const eqArrays = function(Array1,Array2){
-  farz = true;
-  if (Array1.length !== Array2.length){
-      farz = false;
+
+const eqArrays = function(Array1,Array2) {
+  let farz = true;
+  if (Array1.length !== Array2.length) {
+    farz = false;
   } else {
-      for (i=0; i<Array1.length;i++)
-      if (Array1[i]!==Array2[i]) {
-          farz = false;
-        
+    for (let i = 0; i < Array1.length; i++) {
+      if (Array1[i] !== Array2[i]) {
+        farz = false;
       }
-  
+    }
   }
   return farz;
-}
+};
 
+module.exports = eqArrays;
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
