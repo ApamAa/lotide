d18d5df3daddb6568bc27b1156790e87722d7910
ApamAa <apama.jamshidi@gmail.com>
2019-12-02 22:09:28 +0000

    tail.js

--- tail.js
+++ tail.js
@@ -1,8 +1,15 @@
-const tail = function(Array) {
+
+/*const tail = function(Array) {
   let output = [];
   for (let i = 1; i < Array.length; i++) {
     output.push(Array[i]);
   }
   return output;
 };
-console.log(tail([1,2,5,6,3]));
+console.log(tail[1,2,5,6,3]);*/
+const tail = function(array) {
+let exceptFirst = array.slice(1); 
+  return exceptFirst;
+}
+
+module.exports = tail;
